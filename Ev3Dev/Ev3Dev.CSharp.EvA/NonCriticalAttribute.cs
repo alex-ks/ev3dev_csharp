e5afa3f78f067edf7a5051526fa74e91594abbe3
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2018-06-05 00:39:24 +0700

    Now NonCriticalAttribute follows new API
    Now NonCriticalAttribute implements IActionTransformer interface

--- Ev3Dev/Ev3Dev.CSharp.EvA/NonCriticalAttribute.cs
+++ Ev3Dev/Ev3Dev.CSharp.EvA/NonCriticalAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ev3Dev.CSharp.EvA.AttributeContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,57 @@ namespace Ev3Dev.CSharp.EvA
     /// Declares thar any exception in the selected method will not take any effect
     /// on program execution. Any exception will be logged.
     /// </summary>
-    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
-    public class NonCriticalAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class NonCriticalAttribute : Attribute, IActionTransformer
     {
         public bool LogExceptions { get; set; } = true;
+
+        public Action TransformAction(
+            string name, 
+            Action action, 
+            object[] attributes, 
+            IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            // todo: add message to resources
+            if (attributes.FirstOrDefault(attr => attr is CriticalAttribute) != null)
+                throw new ArgumentException("Method must have only one Critical or NonCritical attribute");
+
+            if (LogExceptions)
+                return () =>
+                {
+                    try { action(); }
+                    catch (Exception e) { Console.Error.WriteLine(e); }
+                };
+
+            return () =>
+            {
+                try { action(); }
+                catch (Exception) { }
+            };
+        }
+
+        public Func<Task> TransformAsyncAction(
+            string name, 
+            Func<Task> action, 
+            object[] attributes, 
+            IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            // todo: add message to resources
+            if (attributes.FirstOrDefault(attr => attr is CriticalAttribute) != null)
+                throw new ArgumentException("Method must have only one Critical or NonCritical attribute");
+
+            if (LogExceptions)
+                return async () =>
+                {
+                    try { await action(); }
+                    catch (Exception e) { Console.Error.WriteLine(e); }
+                };
+
+            return async () =>
+            {
+                try { await action(); }
+                catch (Exception) { }
+            };
+        }
     }
 }
