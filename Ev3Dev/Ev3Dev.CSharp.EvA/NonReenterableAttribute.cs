e7835b6fc3b7b8360133527813c4f66ec94bfc9a
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2018-06-05 00:54:24 +0700

    Start reimplementation of NonReenterableAttribute
    Async part is not implemented for now

--- Ev3Dev/Ev3Dev.CSharp.EvA/NonReenterableAttribute.cs
+++ Ev3Dev/Ev3Dev.CSharp.EvA/NonReenterableAttribute.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Ev3Dev.CSharp.EvA.AttributeContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ev3Dev.CSharp.EvA
@@ -11,9 +13,45 @@ namespace Ev3Dev.CSharp.EvA
     /// Declares that method is not reenterable. For default model building (<see cref="ModelParser"/>) only makes 
     /// sense for async methods.
     /// </summary>
-    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
-    public class NonReenterableAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class NonReenterableAttribute : Attribute, IActionTransformer
     {
         public bool DiscardRepeated { get; set; } = true;
+
+        public Action TransformAction(string name, Action action, object[] attributes, IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            var lockGuard = new object();
+
+            if (DiscardRepeated)
+                return () =>
+                {
+                    if (!Monitor.TryEnter(lockGuard))
+                        return;
+
+                    try { action(); }
+                    finally { Monitor.Exit(lockGuard); }
+                };
+
+            return () => { lock (lockGuard) { action(); } };
+        }
+
+        public Func<Task> TransformAsyncAction(string name, Func<Task> action, object[] attributes, IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            var lockGuard = new object();
+
+            // check correctness with monitor
+            // looks like invalid
+            if (DiscardRepeated)
+                return async () =>
+                {
+                    if (!Monitor.TryEnter(lockGuard))
+                        return;
+
+                    try { await action(); }
+                    finally { Monitor.Exit(lockGuard); }
+                };
+
+            throw new NotImplementedException();
+        }
     }
 }
