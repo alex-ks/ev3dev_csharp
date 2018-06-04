7392c9ef9bfb506ba89880d40ca34267da241174
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2018-06-05 00:27:25 +0700

    Now CriticalAttribute follows new API
    Now CriticalAttribute implements IActionTransformer attribute. Also
    added LoopInterruptedException as conventional marker of loop end

--- Ev3Dev/Ev3Dev.CSharp.EvA/CriticalAttribute.cs
+++ Ev3Dev/Ev3Dev.CSharp.EvA/CriticalAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ev3Dev.CSharp.EvA.AttributeContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,61 @@ namespace Ev3Dev.CSharp.EvA
     /// <summary>
     /// Declares thar any exception in the selected method will terminate all program.
     /// </summary>
-    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
-    public class CriticalAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class CriticalAttribute : Attribute, IActionTransformer
     {
         public bool LogExceptions { get; set; } = true;
+
+        public Action TransformAction(
+            string name, 
+            Action action, 
+            object[] attributes, 
+            IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            // todo: add message to resources
+            if (attributes.FirstOrDefault(attr => attr is NonCriticalAttribute) != null)
+                throw new ArgumentException("Method must have only one Critical or NonCritical attribute");
+
+            if (LogExceptions)
+                return () =>
+                {
+                    try { action(); }
+                    catch (LoopInterruptedException e) { Console.Error.WriteLine(e); throw; }
+                    catch (Exception e) { Console.Error.WriteLine(e); throw new LoopInterruptedException(e); }
+                };
+
+            return () =>
+            {
+                try { action(); }
+                catch (LoopInterruptedException) { throw; }
+                catch (Exception e) { throw new LoopInterruptedException(e); }
+            };
+        }
+
+        public Func<Task> TransformAsyncAction(
+            string name, 
+            Func<Task> action, 
+            object[] attributes, 
+            IReadOnlyDictionary<string, PropertyStorage> properties)
+        {
+            // todo: add message to resources
+            if (attributes.FirstOrDefault(attr => attr is NonCriticalAttribute) != null)
+                throw new ArgumentException("Method must have only one Critical or NonCritical attribute");
+
+            if (LogExceptions)
+                return async () =>
+                {
+                    try { await action(); }
+                    catch (LoopInterruptedException e) { Console.Error.WriteLine(e); throw; }
+                    catch (Exception e) { Console.Error.WriteLine(e); throw new LoopInterruptedException(e); }
+                };
+
+            return async () =>
+            {
+                try { await action(); }
+                catch (LoopInterruptedException) { throw; }
+                catch (Exception e) { throw new LoopInterruptedException(e); }
+            };
+        }
     }
 }
---
+++ Ev3Dev/Ev3Dev.CSharp.EvA/LoopInterruptedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev3Dev.CSharp.EvA
+{
+    public class LoopInterruptedException : AggregateException
+    {
+        // todo: add message to resources
+        public LoopInterruptedException() : base("Loop was interrupted and must be stopped") { }
+
+        public LoopInterruptedException(string message) : base(message) { }
+
+        public LoopInterruptedException(params Exception[] innerExceptions) : base(innerExceptions) { }
+    }
+}
