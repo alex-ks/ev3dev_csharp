c2dac6da3d26c77511587e42d5f68e349337fe42
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2018-10-07 12:00:21 +0700

    Implement global exception handlers.

--- Ev3Dev/Ev3Dev.CSharp.EvA/EverythingIsCriticalAttribute.cs
+++ Ev3Dev/Ev3Dev.CSharp.EvA/EverythingIsCriticalAttribute.cs
@@ -16,15 +16,43 @@ namespace Ev3Dev.CSharp.EvA
             if (loopAttributes.Where(attr => attr is EverythingIsNonCriticalAttribute).FirstOrDefault() != null)
                 throw new InvalidOperationException("Ambiguous definition: EverythingIsNonCriticalAttribute is set too");
 
+            var guarder = new CriticalAttribute();
+
             var guardedActions = new Dictionary<string, (Action, object[])>();
             foreach (var entry in contents.Actions)
-                guardedActions[entry.Key] = entry.Value;
+                if (entry.Value.attributes.Where(attr => attr is CriticalAttribute || attr is NonCriticalAttribute)
+                                          .FirstOrDefault() != null)
+                    guardedActions[entry.Key] = entry.Value;
+                else
+                {
+                    var guardedAction = guarder.TransformAction(entry.Key, 
+                                                                entry.Value.action, 
+                                                                entry.Value.attributes, 
+                                                                contents.Properties);
+                    guardedActions[entry.Key] = (guardedAction, entry.Value.attributes);
+                }
+                    
 
             var guardedAsyncs = new Dictionary<string, (Func<Task>, object[])>();
             foreach (var entry in contents.AsyncActions)
-                guardedAsyncs[entry.Key] = entry.Value;
+                if (entry.Value.attributes.Where(attr => attr is CriticalAttribute || attr is NonCriticalAttribute)
+                                          .FirstOrDefault() != null)
+                    guardedAsyncs[entry.Key] = entry.Value;
+                else
+                {
+                    var guardedAction = guarder.TransformAsyncAction(entry.Key,
+                                                                     entry.Value.action,
+                                                                     entry.Value.attributes,
+                                                                     contents.Properties);
+                    guardedAsyncs[entry.Key] = (guardedAction, entry.Value.attributes);
+                }
 
-            throw new NotImplementedException();
+            return new LoopContents
+            {
+                Properties = contents.Properties,
+                Actions = guardedActions,
+                AsyncActions = guardedAsyncs
+            };
         }
     }
 }
--- Ev3Dev/Ev3Dev.CSharp.EvA/EverythingIsNonCriticalAttribute.cs
+++ Ev3Dev/Ev3Dev.CSharp.EvA/EverythingIsNonCriticalAttribute.cs
@@ -15,7 +15,44 @@ namespace Ev3Dev.CSharp.EvA
             // todo: add message to resources
             if (loopAttributes.Where(attr => attr is EverythingIsNonCriticalAttribute).FirstOrDefault() != null)
                 throw new InvalidOperationException("Ambiguous definition: EverythingIsNonCriticalAttribute is set too");
-            throw new NotImplementedException();
+
+            var guarder = new NonCriticalAttribute();
+
+            var guardedActions = new Dictionary<string, (Action, object[])>();
+            foreach (var entry in contents.Actions)
+                if (entry.Value.attributes.Where(attr => attr is CriticalAttribute || attr is NonCriticalAttribute)
+                                          .FirstOrDefault() != null)
+                    guardedActions[entry.Key] = entry.Value;
+                else
+                {
+                    var guardedAction = guarder.TransformAction(entry.Key,
+                                                                entry.Value.action,
+                                                                entry.Value.attributes,
+                                                                contents.Properties);
+                    guardedActions[entry.Key] = (guardedAction, entry.Value.attributes);
+                }
+
+
+            var guardedAsyncs = new Dictionary<string, (Func<Task>, object[])>();
+            foreach (var entry in contents.AsyncActions)
+                if (entry.Value.attributes.Where(attr => attr is CriticalAttribute || attr is NonCriticalAttribute)
+                                          .FirstOrDefault() != null)
+                    guardedAsyncs[entry.Key] = entry.Value;
+                else
+                {
+                    var guardedAction = guarder.TransformAsyncAction(entry.Key,
+                                                                     entry.Value.action,
+                                                                     entry.Value.attributes,
+                                                                     contents.Properties);
+                    guardedAsyncs[entry.Key] = (guardedAction, entry.Value.attributes);
+                }
+
+            return new LoopContents
+            {
+                Properties = contents.Properties,
+                Actions = guardedActions,
+                AsyncActions = guardedAsyncs
+            };
         }
     }
 }
