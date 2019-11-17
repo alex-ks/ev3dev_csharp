ca7926fd4ece0e0ac6cc3b4c94e549014477d5e2
Alexander Komissarov <komissarov.allex@yandex.ru>
2019-11-18 01:48:02 +0700

    Update ports addresses to the last Ev3Dev release.

--- Ev3Dev/src/Ev3Dev.CSharp.BasicDevices/Ports.cs
+++ Ev3Dev/src/Ev3Dev.CSharp.BasicDevices/Ports.cs
@@ -2,48 +2,50 @@
 
 namespace Ev3Dev.CSharp.BasicDevices
 {
-	public enum InputPort
-	{
-		In1, In2, In3, In4
-	}
+    public enum InputPort
+    {
+        In1, In2, In3, In4
+    }
 
-	public enum OutputPort
-	{
-		OutA, OutB, OutC, OutD
-	}
+    public enum OutputPort
+    {
+        OutA, OutB, OutC, OutD
+    }
 
-	public static class PortsHelper
-	{
-		private static readonly IDictionary<OutputPort, string> OutputPortNames;
-		private static readonly IDictionary<InputPort, string> InputPortNames;
+    public static class PortsHelper
+    {
+        private const string PortPrefix = "ev3-ports:";
 
-		static PortsHelper( )
-		{
-			OutputPortNames = new Dictionary<OutputPort, string>
-			{
-				{ OutputPort.OutA, "outA" },
-				{ OutputPort.OutB, "outB" },
-				{ OutputPort.OutC, "outC" },
-				{ OutputPort.OutD, "outD" }
-			};
+        private static readonly IDictionary<OutputPort, string> OutputPortNames;
+        private static readonly IDictionary<InputPort, string> InputPortNames;
 
-			InputPortNames = new Dictionary<InputPort, string>
-			{
-				{ InputPort.In1, "in1" },
-				{ InputPort.In2, "in2" },
-				{ InputPort.In3, "in3" },
-				{ InputPort.In4, "in4" }
-			};
-		}
+        static PortsHelper()
+        {
+            OutputPortNames = new Dictionary<OutputPort, string>
+            {
+                { OutputPort.OutA, PortPrefix + "outA" },
+                { OutputPort.OutB, PortPrefix + "outB" },
+                { OutputPort.OutC, PortPrefix + "outC" },
+                { OutputPort.OutD, PortPrefix + "outD" }
+            };
 
-		public static string ToStringName( this OutputPort port )
-		{
-			return OutputPortNames[port];
-		}
+            InputPortNames = new Dictionary<InputPort, string>
+            {
+                { InputPort.In1, PortPrefix + "in1" },
+                { InputPort.In2, PortPrefix + "in2" },
+                { InputPort.In3, PortPrefix + "in3" },
+                { InputPort.In4, PortPrefix + "in4" }
+            };
+        }
 
-		public static string ToStringName( this InputPort port )
-		{
-			return InputPortNames[port];
-		}
-	}
+        public static string ToStringName(this OutputPort port)
+        {
+            return OutputPortNames[port];
+        }
+
+        public static string ToStringName(this InputPort port)
+        {
+            return InputPortNames[port];
+        }
+    }
 }
